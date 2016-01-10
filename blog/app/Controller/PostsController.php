ac86bf8948f7e9aa02e772fc410b41593e63554d
MAC <mac@USER-no-MacBook-Air.local>
2016-01-11 03:44:53 +0900

    lesson10

--- blog/app/Controller/PostsController.php
+++ blog/app/Controller/PostsController.php
@@ -1,10 +1,12 @@
 <?php
- 
+
 class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
     
     public function index() {
         $this->set('posts', $this->Post->find('all'));
+        $this->set('title_for_layout', '記事一覧');
     }
- 
+
 }
+
