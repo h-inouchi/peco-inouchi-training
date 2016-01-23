787967410ae9ba08ef2fd1d49a74247333fac677
h-inouchi <hibird2720804@gmail.com>
2016-01-23 14:58:25 +0900

    不具合修正
    同じユーザをフォローするたびにフォローユーザ一覧に表示される名前が増えてしまう不具合を修正

--- blog/app/Controller/FollowUsersController.php
+++ blog/app/Controller/FollowUsersController.php
@@ -4,7 +4,19 @@ class FollowUsersController extends AppController {
     public $helpers = array('Html', 'Form');
 
     public function add($id = null) {
+
+        $followUser = $this->FollowUser->find(
+            'all',
+            array(
+                'conditions' => array(
+                    'user_id' => $this->Auth->user('id'),
+                    'follow_user_id' => $id
+                )
+            )
+        );
+
         if ($this->request->is('post')) {
+            $this->request->data['FollowUser']['id'] = $followUser[0]['FollowUser']['id'];
             $this->request->data['FollowUser']['user_id'] = $this->Auth->user('id');
             $this->request->data['FollowUser']['follow_user_id'] = $id;
 
